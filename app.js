69bec9496c6fb284a6fb80b22509d758e010ff5a
unknown <antunsvetic23@gmail.com>
2021-09-24 11:58:57 +0200

    prvi commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const hero = document.querySelector(".hero");
+const slider = document.querySelector(".slider");
+const logo = document.querySelector("#logo");
+const hamburger = document.querySelector(".hamburger");
+const headline = document.querySelector(".headline");
+const headline2 = document.querySelector(".headline2");
+
+const tl = new TimelineMax();
+
+tl.fromTo(hero, 1, {height: "0%"}, {height:"70%", ease: Power2.easeInOut})
+.fromTo(hero, 1.2, {width: "100%"}, {width:"80%", ease: Power2.easeInOut})
+.fromTo(slider, 1.2, {x: "-100%"}, {x: "0%", ease: Power2.easeInOut}, "-=1.2")
+.fromTo(logo, 0.5, {opacity: 0, x:30}, {opacity: 1, x:0}, "-=0.2")
+.fromTo(hamburger, 0.5, {opacity: 0}, {opacity: 1},"-=0.2")
+.fromTo(headline, 0.5, {opacity: 0, x:30}, {opacity: 1, x:-60},"-=1")
+.fromTo(headline2, 0.5, {opacity: 0, x:30}, {opacity: 1, x:-60},"-=1")
